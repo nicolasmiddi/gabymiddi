73efc0f420c5ab766499d1ca852b59d4c2236c81
Nicolas Middi Lab-U <nm@ithk.com.ar>
2020-09-07 01:22:04 +0000

    test ip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,6 +22,11 @@ export default {
     IthContactoComponent
   },
   mounted() {
+
+    this.$http.get(`http://www.geoplugin.net/json.gp`).then(res => {
+      console.log(res.data.geoplugin_request)
+    });
+
     this.$http.get("storage/data/data.json").then(res => {
       this.$store.commit("editInfoGeneral", res.data.info_general);
       this.$store.commit("editRrss", res.data.rrss);
